8d721dcd2f2ff930db2f91c04328c05758a089f4
Yasin Fathollahi <yasin.fathollahi82@gmail.com>
2025-07-04 16:21:36 +0330

    testing

--- src/supabase.js
+++ src/supabase.js
@@ -1,11 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
+console.log('URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
 );
-
-console.log('URL:', import.meta.env.VITE_SUPABASE_URL);
-console.log('KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 async function signup(credentials) {
   return await supabase.auth.signUp(credentials);
